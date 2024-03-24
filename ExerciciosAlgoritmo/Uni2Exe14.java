b8c49973ec31fa6b0f8da2b8287631ffbac08925
Biel <157547002+biel-sebold@users.noreply.github.com>
2024-03-24 19:23:12 -0300

    a

--- ExerciciosAlgoritmo/Uni2Exe14.java
+++ ExerciciosAlgoritmo/Uni2Exe14.java
@@ -9,30 +9,43 @@ public class Uni2Exe14 {
 
         Scanner dig = new Scanner(System.in);
 
-        // Ler o valor inteiro
-        System.out.print("Digite o valor: ");
-        int valor = dig.nextInt();
+        System.out.print("Digite o valor: R$");
+        double valor = dig.nextInt();
 
-        // Array de notas disponíveis
-        int[] notas = {100, 50, 20, 10, 5, 2, 1};
-
-        // Mostrar o valor lido
         System.out.println("Valor lido: " + valor);
 
-        // Mostrar a relação de notas permitida
-        System.out.println("Notas permitidas:");
+        int notas100 = (int)valor/100;
+        valor%=100;
+        int notas50 = (int)(valor % 100)/50;
+        valor%=50;
+        int notas20 = (int)(valor % 50)/20;
+        valor%=20;
+        int notas10 = (int)(valor % 20)/10;
+        valor%=10;
+        int notas5 = (int)(valor % 10)/5;
+        valor%=5;
+        int notas2 = (int)(valor % 5)/2;
+        valor%=2;
+
+        System.out.printf(
+            "Notas de 100: %d\n"+
+            "Notas de 50: %d\n" +
+            "Notas de 20: %d\n" +
+            "Notas de 10: %d\n" +
+            "Notas de 5: %d\n" +
+            "Nota de 2: %d\n",
+            notas100, notas50, notas20, notas10, notas5, notas2);
+
+        dig.close();
+
+          /* 
+        int[] notas = {100, 50, 20, 10, 5, 2};
+
             for (int nota : notas) {
                int quantidadeNotas = valor / nota;
                valor %= nota;
                System.out.println(nota + ": " + quantidadeNotas + " nota(s)");
-        }
-
-        dig.close();
-
-    
-
-
-
+        }*/
         
     }
     
--- ExerciciosAlgoritmo/Uni2Exe15.java
+++ ExerciciosAlgoritmo/Uni2Exe15.java
@@ -9,24 +9,50 @@ public class Uni2Exe15 {
 
         Scanner dig = new Scanner(System.in);
 
-        System.out.print("Digite o valor: ");
+        System.out.print("Digite o valor: R$");
         double valor = dig.nextDouble();
 
-        int[] notas = {100, 50, 20, 10, 5, 2};
-        double[] moedas = {1, 0.50, 0.25, 0.10, 0.05, 0.01}; 
+        //NOTAS
+        int notas100 = (int)valor/100;
+        valor%=100;
+        int notas50 = (int)(valor % 100)/50;
+        valor%=50;
+        int notas20 = (int)(valor % 50)/20;
+        valor%=20;
+        int notas10 = (int)(valor % 20)/10;
+        valor%=10;
+        int notas5 = (int)(valor % 10)/5;
+        valor%=5;
+        int notas2 = (int)(valor % 5)/2;
+        valor%=2;
 
-        System.out.println("Notas permitidas: ");
-            for(int nota : notas){
-               double quantidadeNotas = valor/nota;
-               valor -= quantidadeNotas* nota;
-               System.out.printf("%d nota(s) de R$%.2f\n", quantidadeNotas, nota);
-            }
-
-            for(double moeda : moedas) {
-               int quantidadeMoedas = (int)(valor / moeda);
-               valor -= quantidadeMoedas * moeda;
-               System.out.printf("%d moeda(s) de R$%.2f\n", quantidadeMoedas, moeda);
-            } 
+        //MOEDAS
+        int moedas1 = (int)valor % 2;
+        valor %= 1;
+        int moedas50 = (int)(valor / 0.50);
+        valor %= 0.50;
+        int moedas25 = (int)(valor / 0.25);
+        valor %= 0.25;
+        int moedas10 = (int)(valor / 0.10);
+        valor %= 0.10;
+        int moedas5 = (int)(valor / 0.05);
+        valor %= 0.05;
+        int moedas1Cent = (int)(valor / 0.01);
+        
+        System.out.printf(
+            "Notas de 100: %d\n"+
+            "Notas de 50: %d\n" +
+            "Notas de 20: %d\n" +
+            "Notas de 10: %d\n" +
+            "Notas de 5: %d\n" +
+            "Nota de 2: %d\n" +
+            "Moedas de 1: %d\n" +
+            "Moedas de 50 centavos: %d\n" +
+            "Moedas de 25 centavos: %d\n" +
+            "Moedas de 10 centavos: %d\n" +
+            "Moedas de 5 centavos: %d\n" +
+            "Moedas de 1 centavo: %d\n",
+            notas100, notas50, notas20, notas10, notas5, notas2, moedas1, moedas50, moedas25, moedas10, moedas5, moedas1Cent);
             
         dig.close();   
     }   
